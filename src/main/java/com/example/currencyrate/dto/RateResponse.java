c5a87842da7d6aeec52b7d4173f960fc23967af9
Скавыш Илья Александрович <ilya.skavysh@softclub.by>
2024-07-02 11:38:14 +0300

    Adding Builder annotation to RateReadDto

--- src/main/java/com/example/currencyrate/dto/RateResponse.java
+++ src/main/java/com/example/currencyrate/dto/RateResponse.java
@@ -1,11 +1,13 @@
 package com.example.currencyrate.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@Builder
 public class RateResponse {
     @JsonProperty("Cur_ID") private Long id;
     @JsonProperty("Date") private Date date;
@@ -13,6 +15,4 @@ public class RateResponse {
     @JsonProperty("Cur_Scale") private Long scale;
     @JsonProperty("Cur_Name") private String name;
     @JsonProperty("Cur_OfficialRate") private String officialRate;
-
-
 }
